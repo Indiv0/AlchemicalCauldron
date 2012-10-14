0e6a8300e4ca9ea46229ef43bbcca8327f168b13
Indiv0 <indiv0soft@gmail.com>
2012-10-14 01:14:27 -0400

    Added probabilities

--- src/com/github/Indiv0/AlchemicalCauldron/AlchemicalCauldron.java
+++ src/com/github/Indiv0/AlchemicalCauldron/AlchemicalCauldron.java
@@ -1,11 +1,22 @@
 package com.github.Indiv0.AlchemicalCauldron;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.logging.Level;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AlchemicalCauldron extends JavaPlugin {
-    public final EntityInteractListener entityInteractListener = new EntityInteractListener();
+    public final EntityInteractListener entityInteractListener = new EntityInteractListener(this);
+    
+    private HashMap<Material, Double> inputMaterials = new HashMap<Material, Double>();
+    private HashMap<Material, Double> outputMaterials = new HashMap<Material, Double>();
 
     public void onEnable() {
         // Retrieves an instance of the PluginManager.
@@ -13,9 +24,69 @@ public class AlchemicalCauldron extends JavaPlugin {
 
         // Registers the blockListener with the PluginManager.
         pm.registerEvents(this.entityInteractListener, this);
-
+        
+        loadConfig();
+        
+        //FileConfiguration probabilityConfig = loadConfig("config.yml");
+        loadMaterials(getConfig(), getInputMaterials(), "inputs");
+        loadMaterials(getConfig(), outputMaterials, "outputs");
+        
         // Prints a message to the server confirming successful initialization of the plugin.
         PluginDescriptionFile pdfFile = this.getDescription();
         getLogger().info(pdfFile.getName() + " " + pdfFile.getVersion() + " is enabled.");
+    }
+    
+    private void loadMaterials(FileConfiguration fileConfiguration, HashMap<Material, Double> materials, String section)
+    {
+        ConfigurationSection configSection = fileConfiguration.getConfigurationSection(section);
+        
+        if(configSection == null) {
+            getLogger().log(Level.WARNING, "No keys/values have been defined for the section \"" + section + "\"");
+            return;
+        }
+            
+        Set<String> keyList = configSection.getKeys(false);
+        
+        for(String materialID : keyList) {
+            Material material = Material.matchMaterial(materialID);
+            
+            if(material == null || material == Material.AIR) {
+                getLogger().log(Level.WARNING, "AlCo config contains an invalid key: " + materialID);
+            }
+            else {
+                double val = -1;
+                try {
+                    val = Double.parseDouble((String) fileConfiguration.get(section + "." + materialID));
+                } catch(Exception ex) {
+                    getLogger().log(Level.WARNING, "AlCo config contains an invalid value for key: " + materialID);
+                }
+                if(val < 0 || val > 1)
+                    getLogger().log(Level.WARNING, "AlCo config contains an invalid value for key: " + materialID);
+                
+                materials.put(material, val);
+            }
+        }        
+    }
+    
+    private void loadConfig()
+    {
+        if(!(new File("plugins/AlchemicalCauldron/config.yml").exists())) {
+            // Create some default configuration values.
+            getConfig().addDefault("inputs.2", "0.01");
+            getConfig().addDefault("inputs.cobblestone", "0.2");
+            getConfig().addDefault("outputs.iron_ingot", "0.6");
+            
+            getConfig().options().copyDefaults(true);
+        }
+        
+        saveConfig();
+    }
+
+    public HashMap<Material, Double> getInputMaterials() {
+        return inputMaterials;
+    }
+
+    public HashMap<Material, Double> getOutputMaterials() {
+        return outputMaterials;
     }
 }
--- src/com/github/Indiv0/AlchemicalCauldron/EntityInteractListener.java
+++ src/com/github/Indiv0/AlchemicalCauldron/EntityInteractListener.java
@@ -1,5 +1,9 @@
 package com.github.Indiv0.AlchemicalCauldron;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -11,6 +15,12 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
 public class EntityInteractListener implements Listener {
+    public static AlchemicalCauldron plugin;
+
+    public EntityInteractListener(AlchemicalCauldron instance) {
+        plugin = instance;
+    }
+    
     // Create a method to handle/interact with item throwing events.
     @EventHandler
     public void onEntityInteract(PlayerDropItemEvent event) {
@@ -24,15 +34,41 @@ public class EntityInteractListener implements Listener {
         }
         
         Item thrownItem = event.getItemDrop();
-        ItemStack newItem = new ItemStack(Material.IRON_INGOT, 1);
+        ItemStack thrownItemStack = thrownItem.getItemStack();
+        
+        if(!plugin.getInputMaterials().containsKey(thrownItemStack.getType()))
+            return;
+
+        double inputProbability = plugin.getInputMaterials().get(thrownItemStack.getType());
+        
+        if(Math.random() > inputProbability)
+            return;
+        
+        ItemStack newItem = new ItemStack(getObjectByProbability(plugin.getOutputMaterials().entrySet()), 1);
+        
+        if(newItem.getType() == Material.AIR)
+            return;
         
         thrownItem.setItemStack(newItem);
         thrownItem.teleport(new Location(thrownItem.getWorld(), targetBlock.getX() + 0.5, targetBlock.getY() + 1, targetBlock.getZ() + 0.5));
         
         Vector zero = new Vector();
         zero.setY(0.2);
-        zero.setX(zero.getRandom().getX() * 0.1);
-        zero.setZ(zero.getRandom().getZ() * 0.1);
+        zero.setX(Vector.getRandom().getX() * 0.1);
+        zero.setZ(Vector.getRandom().getZ() * 0.1);
         thrownItem.setVelocity(zero);
+    }
+    
+    private <K> K getObjectByProbability(Set<Entry<K, Double>> set)
+    {
+        while(true) {
+            double probability = Math.random();
+            
+            for(Map.Entry<K, Double> entry : set)
+            {
+                if(probability < entry.getValue()) return entry.getKey();
+                else probability -= entry.getValue();
+            }
+        }
     }
 }
