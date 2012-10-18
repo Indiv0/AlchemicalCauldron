de3dfcb61614b736d60e69725a3c1d474c803a34
Indiv0 <indiv0soft@gmail.com>
2012-10-18 00:10:03 -0400

    Added "used up" item timer

--- src/com/github/Indiv0/AlchemicalCauldron/EntityInteractListener.java
+++ src/com/github/Indiv0/AlchemicalCauldron/EntityInteractListener.java
@@ -48,27 +48,45 @@ public class EntityInteractListener implements Listener {
         
         // If the conversion fails, delete the ItemStack.
         if(Math.random() > inputProbability) {
-            thrownItem.remove();
+            // Sets a timer to despawn the "used up" item.
+            setItemDespawnTimer(thrownItem, 5);
             return;
         }
         
         // If the conversion was successful, makes a new ItemStack with a randomized (based on ratio) output item.
-        ItemStack newItem = new ItemStack(getObjectByProbability(plugin.getOutputMaterials().entrySet()), 1);
+        ItemStack newItemStack = new ItemStack(getObjectByProbability(plugin.getOutputMaterials().entrySet()), 1);
         
         // Possibly unessessary double-check to make sure the material is not AIR?
-        if(newItem.getType() == Material.AIR)
+        if(newItemStack.getType() == Material.AIR)
             return;
         
         // Sets the Item and sets its location to the centre of the CAULDRON.
-        thrownItem.setItemStack(newItem);
-        thrownItem.teleport(new Location(thrownItem.getWorld(), targetBlock.getX() + 0.5, targetBlock.getY() + 1, targetBlock.getZ() + 0.5));
+        Item item = thrownItem.getWorld().dropItem(new Location(thrownItem.getWorld(), targetBlock.getX() + 0.5, targetBlock.getY() + 1, targetBlock.getZ() + 0.5), newItemStack);
+        item.setPickupDelay(20);
+        
+        // Sets a timer to despawn the "used up" item.
+        setItemDespawnTimer(thrownItem, 1);
         
         // Gives the item a slightly randomized vertical velocity.
         Vector zero = new Vector();
-        zero.setY(0.2);
-        zero.setX(Vector.getRandom().getX() * 0.1);
-        zero.setZ(Vector.getRandom().getZ() * 0.1);
-        thrownItem.setVelocity(zero);
+        zero.setY(0.3);
+        zero.setX(Vector.getRandom().getX() * 0.05);
+        zero.setZ(Vector.getRandom().getZ() * 0.05);
+        item.setVelocity(zero);
+    }
+    
+    private void setItemDespawnTimer(final Item item, int seconds)
+    {
+        // Set the item pickup delay to a the requested value + 5s (for safety) so
+        // that it can not be picked up in the meantime.
+        item.setPickupDelay((seconds + 5) * 20);
+        
+        // Creates an Async task, which when run, deletes the item.
+        plugin.getServer().getScheduler().scheduleAsyncDelayedTask(plugin, new Runnable() {
+            public void run() {
+                try { item.remove(); } catch (Exception ex) { }
+            }
+        }, seconds * 20);
     }
     
     private <K> K getObjectByProbability(Set<Entry<K, Double>> set)
