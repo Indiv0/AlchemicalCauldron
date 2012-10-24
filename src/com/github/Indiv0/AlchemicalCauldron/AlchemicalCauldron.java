86a4744e26ac929c6fc63ec3cb8637500b012ac9
Indiv0 <indiv0soft@gmail.com>
2012-10-23 22:15:02 -0400

    Fixed whitespace and removed trailing whitespaces

--- src/com/github/Indiv0/AlchemicalCauldron/AlchemicalCauldron.java
+++ src/com/github/Indiv0/AlchemicalCauldron/AlchemicalCauldron.java
@@ -16,32 +16,35 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.mcstats.MetricsLite;
 
 public class AlchemicalCauldron extends JavaPlugin {
+
     public final ItemDropListener itemDropListener = new ItemDropListener(this);
 
     private HashMap<Material, Double> inputMaterials = new HashMap<Material, Double>();
     private HashMap<Material, Double> outputMaterials = new HashMap<Material, Double>();
-        
-    public void onEnable() {
+
+    @Override
+    public void onEnable () {
         // Retrieves an instance of the PluginManager.
         PluginManager pm = getServer().getPluginManager();
 
         // Registers the blockListener with the PluginManager.
         pm.registerEvents(this.itemDropListener, this);
-        
+
         loadConfig();
-        
+
         loadMaterials(getConfig(), getInputMaterials(), "inputs");
         loadMaterials(getConfig(), getOutputMaterials(), "outputs");
-        
+
         // Enable PluginMetrics.
         enableMetrics();
-        
-        // Prints a message to the server confirming successful initialization of the plugin.
+
+        // Prints a message to the server confirming successful initialization
+        // of the plugin.
         PluginDescriptionFile pdfFile = this.getDescription();
         getLogger().info(pdfFile.getName() + " " + pdfFile.getVersion() + " is enabled.");
     }
 
-    private void enableMetrics()
+    private void enableMetrics ()
     {
         try {
             MetricsLite metrics = new MetricsLite(this);
@@ -50,78 +53,79 @@ public class AlchemicalCauldron extends JavaPlugin {
             System.out.println("An error occured while attempting to connect to PluginMetrics.");
         }
     }
-    
-    private void loadMaterials(FileConfiguration fileConfiguration, HashMap<Material, Double> materials, String section)
+
+    private void loadMaterials (FileConfiguration fileConfiguration, HashMap<Material, Double> materials, String section)
     {
         // Defines the section of the configuration to be searched.
         ConfigurationSection configSection = fileConfiguration.getConfigurationSection(section);
-        
+
         // If the configuration section does not exist, outputs a warning.
-        if(configSection == null) {
+        if (configSection == null) {
             getLogger().log(Level.WARNING, "No keys/values have been defined for the section \"" + section + "\"");
             return;
         }
-        
+
         // Gets all of the keys for the section.
         Set<String> keyList = configSection.getKeys(false);
-        
-        for(String materialID : keyList) {
+
+        for (String materialID : keyList) {
             // Attempts to get the material represented by the key.
             Material material = Material.matchMaterial(materialID);
-            
+
             // If the key is invalid, output as such.
             if (material == null || material == Material.AIR) {
                 getLogger().log(Level.WARNING, "AlCo config contains an invalid key: " + materialID);
                 continue;
             }
-            
+
             // Tries to lead the ratio value for that key.
             double val = -1;
             try {
                 val = Double.parseDouble((String) fileConfiguration.get(section + "." + materialID));
-            } catch(Exception ex) {
+            } catch (Exception ex) {
                 getLogger().log(Level.WARNING, "Config contains an invalid value for key: " + materialID);
             }
-            
+
             // Reduce the precision to 2 decimal places.
             DecimalFormat form = new DecimalFormat();
             form.setMaximumFractionDigits(2);
             form.setMinimumFractionDigits(0);
             val = Double.parseDouble(form.format(val));
-            
+
             if (val < 0 || val > 1)
                 getLogger().log(Level.WARNING, "Config contains an invalid value for key: " + materialID);
-            
-            // Makes sure an item is not being added twice, then adds the material and its value to the cache.
+
+            // Makes sure an item is not being added twice, then adds the
+            // material and its value to the cache.
             if (getOutputMaterials().containsKey(material)) {
                 getLogger().log(Level.WARNING, "Config contains the same material twice. Will not be added again.");
                 continue;
             }
-            
+
             materials.put(material, val);
         }
     }
 
-    private void loadConfig() {
+    private void loadConfig () {
         // If the config.yml already exists, do not create a default one.
-        if(new File("plugins/AlchemicalCauldron/config.yml").exists())
+        if (new File("plugins/AlchemicalCauldron/config.yml").exists())
             return;
-        
+
         // Create some default configuration values.
         getConfig().addDefault("inputs.2", "0.01");
         getConfig().addDefault("inputs.cobblestone", "0.2");
         getConfig().addDefault("outputs.iron_ingot", "0.6");
-        
+
         getConfig().options().copyDefaults(true);
 
         saveConfig();
     }
-    
-    public HashMap<Material, Double> getInputMaterials() {
+
+    public HashMap<Material, Double> getInputMaterials () {
         return inputMaterials;
     }
 
-    public HashMap<Material, Double> getOutputMaterials() {
+    public HashMap<Material, Double> getOutputMaterials () {
         return outputMaterials;
     }
 }
--- src/com/github/Indiv0/AlchemicalCauldron/ItemDropListener.java
+++ src/com/github/Indiv0/AlchemicalCauldron/ItemDropListener.java
@@ -15,68 +15,75 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
 public class ItemDropListener implements Listener {
+
     public static AlchemicalCauldron plugin;
 
-    public ItemDropListener(AlchemicalCauldron instance) {
+    public ItemDropListener (AlchemicalCauldron instance) {
         plugin = instance;
     }
-    
+
     // Create a method to handle/interact with item throwing events.
     @EventHandler
-    public void onItemDrop(PlayerDropItemEvent event) {
+    public void onItemDrop (PlayerDropItemEvent event) {
         // Gets the block targetted by the player.
         Block targetBlock = event.getPlayer().getTargetBlock(null, 1);
-        
+
         // Checks to make sure the block is a CAULDRON.
         try {
-            if(targetBlock.getType() != Material.CAULDRON)
+            if (targetBlock.getType() != Material.CAULDRON)
                 return;
         } catch (NullPointerException ex) {
             return;
         }
-        
+
         // Gets the thrown item and converts it into a usable ItemStack.
         Item thrownItem = event.getItemDrop();
         ItemStack thrownItemStack = thrownItem.getItemStack();
-        
+
         // Checks to make sure the ItemStack contains a valid input material.
-        if(!plugin.getInputMaterials().containsKey(thrownItemStack.getType()))
+        if (!plugin.getInputMaterials().containsKey(thrownItemStack.getType()))
             return;
 
         // Gets the probability for that input item.
         double inputProbability = plugin.getInputMaterials().get(thrownItemStack.getType());
-        
+
         for (int i = 1; i <= thrownItemStack.getAmount(); i++) {
             // If the conversion fails, delete the ItemStack.
-            if(Math.random() > inputProbability) {
+            if (Math.random() > inputProbability) {
                 // Sets a timer to despawn the "used up" item.
                 setItemDespawnTimer(thrownItem, 3);
                 continue;
             }
-            
-            // If the conversion was successful, makes a new ItemStack with a randomized (based on ratio) output item.
+
+            // If the conversion was successful, makes a new ItemStack with a
+            // randomized (based on ratio) output item.
             ItemStack newItemStack = new ItemStack(getObjectByProbability(plugin.getOutputMaterials().entrySet()), 1);
-            
-            // Possibly unessessary double-check to make sure the material is not AIR?
-            if(newItemStack.getType() == Material.AIR)
+
+            // Possibly unessessary double-check to make sure the material is
+            // not AIR?
+            if (newItemStack.getType() == Material.AIR)
                 continue;
-            
-            // Creates the timer which, when completed, will delete the "input" block and create the "output" block.
-            setItemCreationTimer(thrownItem, 
-                    new Location(thrownItem.getWorld(), targetBlock.getX() + 0.5, targetBlock.getY() + 1, targetBlock.getZ() + 0.5), 
+
+            // Creates the timer which, when completed, will delete the "input"
+            // block and create the "output" block.
+            setItemCreationTimer(thrownItem,
+                    new Location(thrownItem.getWorld(), targetBlock.getX() + 0.5, targetBlock.getY() + 1, targetBlock.getZ() + 0.5),
                     newItemStack, 3);
         }
     }
 
-    private void setItemCreationTimer(final Item previousItem, final Location loc, final ItemStack itemStack, final int seconds)
+    private void setItemCreationTimer (final Item previousItem, final Location loc, final ItemStack itemStack, final int seconds)
     {
         // Sets a timer to despawn the "used up" item.
         setItemDespawnTimer(previousItem, seconds);
-        
+
         // Creates an Async task, which when run, creates the new item..
         plugin.getServer().getScheduler().scheduleAsyncDelayedTask(plugin, new Runnable() {
-            public void run() {
-                // Sets the Item and sets its location to the centre of the CAULDRON.
+
+            @Override
+            public void run () {
+                // Sets the Item and sets its location to the centre of the
+                // CAULDRON.
                 Item item = previousItem.getWorld().dropItem(loc, itemStack);
                 item.setPickupDelay(seconds);
 
@@ -89,31 +96,39 @@ public class ItemDropListener implements Listener {
             }
         }, seconds * 20);
     }
-    
-    private void setItemDespawnTimer(final Item item, int seconds)
+
+    private void setItemDespawnTimer (final Item item, int seconds)
     {
-        // Set the item pickup delay to a the requested value + 5s (for safety) so
+        // Set the item pickup delay to a the requested value + 5s (for safety)
+        // so
         // that it can not be picked up in the meantime.
         item.setPickupDelay((seconds + 5) * 20);
-        
+
         // Creates an Async task, which when run, deletes the item.
         plugin.getServer().getScheduler().scheduleAsyncDelayedTask(plugin, new Runnable() {
-            public void run() {
-                try { item.remove(); } catch (Exception ex) { }
+
+            @Override
+            public void run () {
+                try {
+                    item.remove();
+                } catch (Exception ex) {
+                }
             }
         }, seconds * 20);
     }
-    
-    private <K> K getObjectByProbability(Set<Entry<K, Double>> set)
+
+    private <K> K getObjectByProbability (Set<Entry<K, Double>> set)
     {
         // Selects a randomized output value based on its probability.
-        while(true) {
+        while (true) {
             double probability = Math.random();
-            
-            for(Map.Entry<K, Double> entry : set)
+
+            for (Map.Entry<K, Double> entry : set)
             {
-                if(probability < entry.getValue()) return entry.getKey();
-                else probability -= entry.getValue();
+                if (probability < entry.getValue())
+                    return entry.getKey();
+                else
+                    probability -= entry.getValue();
             }
         }
     }
