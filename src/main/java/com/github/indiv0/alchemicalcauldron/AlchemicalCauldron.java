bde50bc663b9cff5e168d3206b4767ac49dfbf8f
Indiv0 <indiv0soft@gmail.com>
2012-11-18 02:55:29 -0500

    Restrutured code directories

--- src/main/java/com/github/indiv0/alchemicalcauldron/AlchemicalCauldron.java
+++ src/main/java/com/github/indiv0/alchemicalcauldron/AlchemicalCauldron.java
@@ -2,7 +2,7 @@
  *
  * @author Indivisible0
  */
-package com.github.Indiv0.AlchemicalCauldron;
+package com.github.indiv0.alchemicalcauldron;
 
 import java.text.DecimalFormat;
 import java.util.HashMap;
--- src/main/java/com/github/indiv0/alchemicalcauldron/ItemDropListener.java
+++ src/main/java/com/github/indiv0/alchemicalcauldron/ItemDropListener.java
@@ -2,7 +2,7 @@
  *
  * @author Indivisible0
  */
-package com.github.Indiv0.AlchemicalCauldron;
+package com.github.indiv0.alchemicalcauldron;
 
 import java.util.Map;
 import java.util.Map.Entry;
